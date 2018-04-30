cd538cbbc72b04bacc96ea7da55568745cece1cb
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-04-29 21:44:58 -0400

    Start front-page layout; fullscreen background

--- template-parts/content-frontpage.php
+++ template-parts/content-frontpage.php
@@ -7,11 +7,20 @@
 
 ?>
 
+<p class="lead">
+	The luscious tragedy of this whole cosmically unlikely existence overwhelms me sometimes. The only solace is to think small, minute by minute, touch to touch. Novel distractions.
+</p>
 
 <h1>GD Hoffman</h1>
 	
-	<p class="lead">
-		GD Hoffman is a fiction writer, film photographer, and former makeup artist.  His past professional exploits include working as a commercial radio producer, ringside MMA camera operator, script reader, screenwriter, filmmaker, video producer, postal worker, political campaigner, marketing copywriter, and cab driver.
-	</p>
+<nav id="site-navigation" class="main-navigation">
+	<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'brutal' ); ?></button>
+	<?php
+		wp_nav_menu( array(
+			'theme_location' => 'menu-1',
+			'menu_id'        => 'primary-menu',
+		) );
+	?>
+</nav><!-- #site-navigation -->	
 	
-	<h2>For all inquiries, bookings, commissions, and collaborations, please contact ... <a href="mailto:gregorydavidhoffman@gmail.com">gregorydavidhoffman@gmail.com</a></h2>
+<!-- Twitter feed -->
